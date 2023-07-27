58c56fbeda032d9856b5888e7852d399c243be88
wer8888 <91093536+plagakit@users.noreply.github.com>
2023-07-26 20:55:45 -0400

    Remove in-source build that accidentally slipped into a commit

--- src/expr/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/thoma/Documents/Projects/CPP/lox-interpreter/src/expr
-# Build directory: C:/Users/thoma/Documents/Projects/CPP/lox-interpreter/src/expr
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
